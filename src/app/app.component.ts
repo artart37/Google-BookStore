bb3b5e25c8efc83ec7dca01898e97ace6eea683f
Aramayis Galichyan <aramayisgalichyan@gmail.com>
2022-03-21 01:48:08 +0400

    Root configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { smoothappearance } from './shared/animations/general-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [smoothappearance],
 })
 export class AppComponent {
   title = 'BookStore';
+  headersigninsignup: boolean = true;
+  centralized!: boolean;
+
+  handler(e: any) {
+    //Check if the router outlet has a property named perfectCentre
+    //This is to ensure the main section is ideally centralised where the corresponding router component has the perfectCentre property
+    e.hasOwnProperty('perfectCentre')
+      ? (this.centralized = true)
+      : (this.centralized = false);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { StructuralModule } from './structural-components/structural.module';
+import { SigninModule } from './shared/components/sign-in/modules/sign-in.module';
 
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, StructuralModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    StructuralModule,
+    SigninModule,
+    AppRoutingModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
