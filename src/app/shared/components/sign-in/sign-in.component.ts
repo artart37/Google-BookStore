97a58468e2855d0a0f5cd924471c22a61f3f5792
Aramayis Galichyan <aramayisgalichyan@gmail.com>
2022-03-24 02:30:56 +0400

    Configure signin

--- src/app/shared/components/sign-in/sign-in.component.ts
+++ src/app/shared/components/sign-in/sign-in.component.ts
@@ -1,27 +1,35 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm, NgModel } from '@angular/forms';
 import { Router } from '@angular/router';
+import { BookCommunicationService } from 'src/app/ui-components/book-store/services/book-communication.service';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css'],
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
   perfectCentre: boolean = true;
   showEmailError: boolean = false;
   hintMessage = 'Please input your username to continue';
   errorMessage = 'Username is empty';
+  signedIn: boolean = false;
   @ViewChild('username', { static: true }) usernameNgmodel!: NgModel;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private bookCommunicationService: BookCommunicationService
+  ) {}
 
+  ngOnInit(): void {
+    this.bookCommunicationService.resetWishList();
+  }
   checkUsernameValidity(): void {
     this.showEmailError = !this.usernameNgmodel.control.valid;
   }
 
-  signinAction(userNameForm: NgForm): void {
-    this.router.navigate(['book-store'], {
+  signInAction(userNameForm: NgForm): void {
+    this.router.navigate(['book-store/dashboard'], {
       queryParams: {
         username: userNameForm.control.value.username,
       },
